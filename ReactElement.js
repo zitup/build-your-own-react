e0727d984cbeb1b8e2735b9027ee742f9629c942
zitup <yitongzhao@163.com>
2021-05-19 09:55:00 +0800

    Fiber 1

--- ReactElement.js
+++ ReactElement.js
@@ -22,26 +22,78 @@ function createTextElement(text) {
   }
 }
 
-function render(element, container) {
+function createDom(fiber) {
   // const dom = document.createElement(element.type)
   // 兼容 TEXT_ELEMENT 类元素
-  const dom = element.type === 'TEXT_ELEMENT'
+  const dom = fiber.type === 'TEXT_ELEMENT'
     ? document.createTextNode('')
-    : document.createElement(element.type)
+    : document.createElement(fiber.type)
 
   // assign dom props
   const isProperty = key => key !== 'children'
-  Object.keys(element.props).filter(isProperty).forEach(key => {
-    dom[key] = element.props[key]
+  Object.keys(fiber.props).filter(isProperty).forEach(key => {
+    dom[key] = fiber.props[key]
   })
 
-  // 遍历处理子元素
-  element.props.children.forEach(child =>
-    render(child, dom)
-  )
-
-  container.appendChild(dom)
+  return dom
 }
+
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element]
+    }
+  }
+}
+
+let nextUnitOfWork = null
+
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+
+  const elements = fiber.props.children
+  let index = 0
+  let prevSibling = null
+
+  while (index < elements.length) {
+    const element = elements[index]
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    }
+
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber
+    index++
+  }
+}
+
 
 
 const Didact = {
